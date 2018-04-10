5539c02a992d53be69355a5e4d01ffdaaa6c16c1
bigrocs <bigrocs@qq.com>
2018-04-10 20:00:19 +0800

    完成缓存过期

--- src/store/cache.js
+++ src/store/cache.js
@@ -4,17 +4,21 @@ import localForage from 'localforage'
 export const get = (key) => {
   return new Promise((resolve, reject) => {
       localForage.getItem(key).then((value) => {
-          resolve(value)
+          let data = value.data
+          if (value.minutes < new Date().getTime()) {
+            data = false
+          }
+          resolve(data)
       }).catch((err) => {
           reject(err)
       })
   })
 }
 
-export const set = (key, minutes = 0, value) => {
+export const set = (key, value, minutes = 0) => {
   return new Promise((resolve, reject) => {
       localForage.setItem(key, {
-        value: value,
+        data: value,
         minutes: expiryTime(minutes)
       }).then((value) => {
           resolve(value)
@@ -26,7 +30,7 @@ export const set = (key, minutes = 0, value) => {
 //确认项目是否存在
 export const has = (key) => {
   return new Promise((resolve, reject) => {
-      localForage.getItem(key).then((value) => {
+      get(key).then((value) => {
           let has = value? true: false
           resolve(has)
       }).catch((err) => {
@@ -36,14 +40,14 @@ export const has = (key) => {
 }
 
 //请求的数据不存在时，程序能为你存储默认值
-export const remember = async (key,  minutes = 0, value) => {
+export const remember = async (key, value, minutes = 0) => {
   if (await has(key)) {
     return get(key)
   }else{
-    return set(key, minutes, value)
+    return set(key, value, minutes)
   }
-
 }
+
 // 过期时间
 const expiryTime = (minutes) => {
   if (minutes) {
