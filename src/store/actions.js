91b735da33a7892a2a5d4b51d93c62be524fc87c
bigrocs <bigrocs@qq.com>
2018-04-10 17:35:15 +0800

    新增缓存系统

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,3 +1,11 @@
 import * as types from './mutation-types'
-import localForage from 'localforage'
+import * as cache from './cache'
 import axios from 'axios'
+
+// console.log(cache.set('aaa','bb'));
+//
+// cache.get('aaa').then((value) => {
+//     console.log(value)
+// })
+// cache.has('aaa')
+console.log(cache.remember('aaa',0,'ccc'));
---
+++ src/store/cache.js
@@ -0,0 +1,46 @@
+import localForage from 'localforage'
+
+//从缓存中获取数据
+export const get = (key) => {
+  return new Promise((resolve, reject) => {
+      localForage.getItem(key).then((value) => {
+          resolve(value)
+      }).catch((err) => {
+          reject(err)
+      })
+  })
+}
+
+export const set = (key, minutes = 0, value) => {
+  return new Promise((resolve, reject) => {
+      localForage.setItem(key, {
+        value: value,
+        minutes: minutes
+      }).then((value) => {
+          resolve(value)
+      }).catch((err) => {
+          reject(err)
+      })
+  })
+}
+//确认项目是否存在
+export const has = (key) => {
+  return new Promise((resolve, reject) => {
+      localForage.getItem(key).then((value) => {
+          let has = value? true: false
+          resolve(has)
+      }).catch((err) => {
+          reject(false)
+      })
+  })
+}
+
+//请求的数据不存在时，程序能为你存储默认值
+export const remember = async (key,  minutes = 0, value) => {
+  if (await has(key)) {
+    return get(key)
+  }else{
+    return set(key, minutes, value)
+  }
+
+}
