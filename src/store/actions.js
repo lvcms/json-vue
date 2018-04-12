588029898c7f388fe3aaacc11f648bba3dbbc72b
bigrocs <bigrocs@qq.com>
2018-04-12 09:00:52 +0800

    独立 cache

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,11 +1,12 @@
 import * as types from './mutation-types'
-import * as cache from './cache'
+import Cache from 'lf-cache'
 import axios from 'axios'
 
-// console.log(cache.set('aaa','bb'));
+console.log(Cache.set('aaa','bb'));
 //
 // cache.get('aaa').then((value) => {
 //     console.log(value)
 // })
 // cache.has('aaa')
-console.log(cache.remember('aaa',0,'ccc'));
+// console.log(cache.remember('aaa','ccc',0));
+console.log(Cache.get('aaa'));
