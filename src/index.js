3c34e2530697f3f0b480f5c05e834aef6b35eb18
bigrocs <bigrocs@qq.com>
2018-04-19 20:32:38 +0800

    启用阿波罗

--- src/index.js
+++ src/index.js
@@ -4,6 +4,27 @@ import router from './router'
 import store from './store'
 import 'font-awesome/css/font-awesome.css'
 
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import VueApollo from 'vue-apollo'
+
+const httpLink = new HttpLink({
+  uri: window.config.graphql,
+})
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+})
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
+
+Vue.use(VueApollo)
+
 Vue.config.productionTip = false
 
 export default {
@@ -12,6 +33,7 @@ export default {
       el: '#app',
       router,
       store,
+      provide: apolloProvider.provide(),
       render: h => h(App)
     })
   },
