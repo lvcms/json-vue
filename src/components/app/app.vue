4b9098ac00d1f8fe89a201b785291a7ae254afed
bigrocs <bigrocs@qq.com>
2018-04-23 20:51:37 +0800

    delete main

--- src/components/app/app.vue
+++ src/components/app/app.vue
@@ -1,7 +1,5 @@
 <template>
-  <div id="main" class="app-main">
-    <router-view></router-view>
-  </div>
+  <router-view></router-view>
 </template>
 
 <script>
@@ -50,12 +48,6 @@ export default {
 </script>
 <style>
 html,body{
-    width: 100%;
-    height: 100%;
-    background: #f0f0f0;
-    overflow: hidden;
-}
-.app-main{
     width: 100%;
     height: 100%;
 }
