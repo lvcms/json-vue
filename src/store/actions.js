a4a73bd13dae21067b0bb38113525e4484df22f2
bigrocs <bigrocs@qq.com>
2018-07-12 15:31:16 +0800

    修正格式错误

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,15 +1,10 @@
-import * as types from './mutation-types'
-import { Message } from 'iview'
-
-export const graphqlError = ({ commit,state }, value) => {
-  let content = value.replace(/GraphQL error: /, "")
+export const graphqlError = ({ commit, state }, value) => {
+  let content = value.replace(/GraphQL error: /, '')
   switch (content) {
     case 'Unauthenticated':
       return '未经认证的'
-      break;
     case 'Unauthorized':
       return '未经授权的'
-      break;
     default:
       return content
   }
